8e85e5cb520c9452a5e6aadab83ecd31ee5fa2a7
thaakeno <thaakeno46@gmail.com>
2026-08-16 11:37:44 +0200

    Format guided T2I runtime imports

--- h3studio/legacy_t2i_multiguide.py
+++ h3studio/legacy_t2i_multiguide.py
@@ -211,8 +211,9 @@ def _install_pipeline_patch() -> None:
                 diagnostics,
             )
 
-        from .nodes.image_runtime import _prompt_warning
         import node_helpers
+
+        from .nodes.image_runtime import _prompt_warning
 
         width, height = int(studio_context.width), int(studio_context.height)
         prompt = str(studio_context.prompt)
